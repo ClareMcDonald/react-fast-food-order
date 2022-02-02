f57520a84ea2d423321eb0f75cbba37396d314bc
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-01 16:50:26 -0800

    added drink dropdown

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import { useState } from 'react';
 import NameInput from './NameInput';
 import FoodImages from './FoodImages';
 import FoodDropdown from './FoodDropdown';
+import DrinkDropdown from './DrinkDropdown';
 import './App.css';
 
 function App() {
   const [foodId, setFoodId] = useState(1);
   const [sideId, setSideId] = useState(1);
-  const [drinkId, setdrinkId] = useState(1);
+  const [drinkId, setDrinkId] = useState(1);
   const [instructions, setInstructions] = useState(['']);
   const [orderName, setOrderName] = useState('');
 
   return (
     <div className="App">
       <h2>Order for: {orderName}</h2>
-      <FoodImages foodId={foodId}/>
+      <FoodImages foodId={foodId} drinkId={drinkId} />
 
       <div className="order-from">
         <NameInput setOrderName={setOrderName} />
-        <FoodDropdown setFoodId={setFoodId}/> 
+        <FoodDropdown setFoodId={setFoodId} /> 
+        <DrinkDropdown setDrinkId={setDrinkId} />
       </div>
     </div>
   );
--- src/FoodImages.js
+++ src/FoodImages.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
 
-export default function FoodImages({ foodId }) {
+export default function FoodImages({ foodId, drinkId }) {
   return <div className="images">
     <img className="food-image" src={`food-${foodId}.png`} />
+    <img className="food-image" src={`drink-${drinkId}.png`}/>
   </div>;
 }
